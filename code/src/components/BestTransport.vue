1b4247e2e4fdf7cb0222bb4b622b1292865e1fdb
taleshrocha <taleshrochaz@gmail.com>
2023-06-11 22:01:05 -0300

    Fastest freight

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -4,22 +4,12 @@
       <b-form @submit="onSubmit">
         <h1>Insira o destino e o peso</h1>
         <b-form-group id="input-group-1" label="Destino" label-for="input-1">
-          <b-form-select
-            id="input-1"
-            v-model="form.cityName"
-            :options="cities"
-            placeholder="Selecione o destino"
-            required
-          />
+          <b-form-select id="input-1" v-model="form.cityName" :options="cities" placeholder="Selecione o destino"
+            required />
         </b-form-group>
 
         <b-form-group id="input-group-2" label="Peso" label-for="input-2">
-          <b-form-input
-            id="input-2"
-            v-model="form.loadWeight"
-            placeholder="Insira o peso"
-            required
-          />
+          <b-form-input id="input-2" v-model="form.loadWeight" placeholder="Insira o peso" required />
         </b-form-group>
 
         <b-button type="submit" variant="primary">Analisar</b-button>
@@ -28,20 +18,10 @@
 
     <b-col>
       <b-container fluid="true" v-show="showInfo">
-        <PriceCard
-          title="Frete com menor valor"
-          :shipping="cheapestFreight.name"
-          :time="cheapestFreight.lead_time"
-          :price="cheapestFreight[costTransport]"
-          iconName="cash-coin"
-        />
-        <PriceCard
-          title="Frete mais rápido"
-          shipping="Blah"
-          time="6h"
-          price="6"
-          iconName="clock"
-        />
+        <PriceCard title="Frete com menor valor" :shipping="cheapestFreight.name" :time="cheapestFreight.lead_time"
+          :price="cheapestFreight[costTransport]" iconName="cash-coin" />
+        <PriceCard title="Frete mais rápido" :shipping="fastestFreight.name" :time="fastestFreight.lead_time"
+          :price="fastestFreight[costTransport]" iconName="clock" />
       </b-container>
     </b-col>
   </b-row>
@@ -87,7 +67,12 @@ export default {
       this.freightsToCity = this.freights.filter(
         (freight) => freight.city === this.form.cityName
       );
+      // TODO: if cheapest as fastest is the same lead time. Then: show cheapest
       this.getCheapestFreight();
+      this.getFastestFreight();
+      if (this.cheapestFreight.lead_time === this.fastestFreight.lead_time) {
+        this.fastestFreight = this.cheapestFreight
+      }
       this.showInfo = true
       //alert(JSON.stringify(this.form));
     },
@@ -104,7 +89,14 @@ export default {
           : min;
       });
     },
-    getFastestFreight() {},
+    getFastestFreight() {
+      this.fastestFreight = this.freightsToCity.reduce((min, freight) => {
+        return parseFloat(freight.lead_time.replace("h", "")) <
+          parseFloat(min.lead_time.replace("h", ""))
+          ? freight
+          : min;
+      });
+    },
   },
 };
 </script>
